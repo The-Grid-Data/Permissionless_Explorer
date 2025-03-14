80b53d10106f2d57bf254b0138416616354aa731
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2025-03-14 16:49:56 +0100

    Update hero.tsx

--- components/containers/hero/hero.tsx
+++ components/containers/hero/hero.tsx
@@ -4,15 +4,14 @@ export const Hero = () => {
   return (
     <section className="container mx-auto flex max-w-4xl flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-12 lg:pb-12">
       <Badge className="my-2 text-center" variant="secondary">
-        Focused on Solana.
+        An Alpha product. 
       </Badge>
 
       <h1 className="scroll-m-20 text-balance text-center text-4xl font-extrabold leading-tight tracking-tight lg:text-5xl lg:leading-[1.1]">
-        THIS NO LONGER WORKS - Go to <a href="https://solana.thegrid.id/?tags=18">our Solana Explorer</a> to view the data
+        THIS NO LONGER WORKS - Go to <a href="https://solana.thegrid.id/?tags=18" className="underline decoration-2">our Solana Explorer</a> to view the data
       </h1>
       <p className="max-w-xl text-center text-lg font-light text-foreground">
-        This data is pulled from The Grid and is freely available via this page
-        or via our API.
+        This page is running an old version of our explorer but the data is still there!
       </p>
     </section>
   );
